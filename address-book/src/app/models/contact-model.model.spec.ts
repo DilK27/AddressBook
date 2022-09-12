efee3d21c0b96534ba8bd10712983f44ed355858
DilK27 <dil.khahra@gmail.com>
2022-09-12 14:41:49 -0500

    updates

--- address-book/src/app/models/contact-model.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ContactModel } from './contact-model.model';
-
-describe('ContactModel', () => {
-  it('should create an instance', () => {
-    expect(new ContactModel()).toBeTruthy();
-  });
-});
